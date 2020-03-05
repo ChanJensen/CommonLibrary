9aa0d71e9633bb51db84f4bbaadd94191015e21b
JensenChen <281743346@qq.com>
2020-03-05 15:51:02 +0800

    Initial commit

---
+++ app/src/main/java/com/jensenchen/commonlibrary/CommonManager.java
@@ -0,0 +1,15 @@
+package com.jensenchen.commonlibrary;
+
+import android.util.Log;
+
+import javax.security.auth.login.LoginException;
+
+/**
+ * 私人代码库
+ */
+public class CommonManager {
+
+    public static void test(){
+        Log.e("123","456");
+    }
+}
